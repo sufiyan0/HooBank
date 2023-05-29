9e2093c07a89a8eb79fee751361a92ff4e0e2fa8
Sufiyan_Ahmed <sufiyanlaiq4@gmail.com>
2023-05-30 00:45:23 +0500

    business section completed

--- hoo-bank/src/app/components/Business.tsx
+++ hoo-bank/src/app/components/Business.tsx
@@ -2,14 +2,41 @@ import { features } from '../constents'
 import React from 'react'
 import styles, { layout } from '../style'
 import Button from './Button'
+import Image from 'next/image'
+// 'use client'
 
-const FeaturesCart = () => (
-  <div> </div>
+// const FeaturesCart = ({ }) => (
+//   
+
+// )
+
+const FeatureCard = ({index,icon ,title,content}:any) => (
+ 
+    <div className={`flex flex-row p-6 feature-card rounded-[20px] ${index !==features.length - 1 ? "mb-6" : "mb-0" }`}>
+
+      <div className={` w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+        <Image src={icon} alt='icons'/>
+      </div> 
+      
+      <div className={`flex flex-1 flex-col ml-3 `}>
+          <h4 className='font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1' >
+            {title}
+          </h4>
+          <p className='font-poppins font-semibold text-dimWhite text-[14px] leading-[20px] mb-1'>
+            {content}
+          </p>
+      </div>
+
+
+  </div>
+
+
 )
 
-const Business = () =>  (
 
-   <section id='features' className={`${layout.section}`}>
+const Business = () => (
+
+  <section id='features' className={`${layout.section}`}>
     <div className={`${layout.sectionInfo}`}>
       <h2 className={`${styles.heading2}`}>
         You do the business, <br className="sm:block hidden" /> we’ll handle
@@ -21,26 +48,26 @@ const Business = () =>  (
         of credit cards on the market.
       </p>
 
-      <Button  />
+      <Button />
 
     </div>
 
     <div className={`${layout.sectionImg} flex-col`}>
-    {
-      features.map((feature,index) => (
+      {
+        features.map((feature, index) => (
 
-        <FeaturesCart  key={feature.id} {...feature} index={`${index}`} /> 
+          <FeatureCard key={feature.id} {...feature} index={index} />
 
-      ))
+        ))
 
-      
-    }
+
+      }
 
     </div>
 
 
-   </section>
-  )
+  </section>
+)
 
 
 export default Business
