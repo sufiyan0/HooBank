8ceafd73232c5147b7ca0719e3a1f126c9c379ff
Sufiyan_Ahmed <sufiyanlaiq4@gmail.com>
2023-05-31 02:56:50 +0500

    button updated & cat done

--- hoo-bank/src/app/components/Button.tsx
+++ hoo-bank/src/app/components/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = () => 
   (
-    <button className={`mt-6 px-6 py-4 font-poppins font-semibold text-[16px] rounded-lg bg-blue-gradient text-primary`}>
+    <button className={`mt-6 px-6 py-4 font-poppins font-semibold text-[16px] rounded-lg bg-blue-gradient text-primary hover:scale-105`}>
         Get Started
     </button>
   )
