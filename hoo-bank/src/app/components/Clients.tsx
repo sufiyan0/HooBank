8ca6c1900194e3c619faf9a1b9b86b2266dd6f8a
Sufiyan_Ahmed <sufiyanlaiq4@gmail.com>
2023-05-31 02:45:17 +0500

    client section completed

--- hoo-bank/src/app/components/Clients.tsx
+++ hoo-bank/src/app/components/Clients.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
+import { clients } from "../constents";
+import styles from "../style";
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div key={client.id} className={`${styles.flexCenter} flex-1 min-w-[120px] sm:min-w-[190px] m-5`}>
 
-const Clients = () => {
-  return (
-    <div>
-      Clients
+          <Image src={client.logo} alt='client' className="sm:w-[192px] w-[100px] object-contain" />
+        </div>
+      ))}
     </div>
-  )
-}
+
+  </section>
+)
 
 export default Clients
--- hoo-bank/src/app/components/Testimonials.tsx
+++ hoo-bank/src/app/components/Testimonials.tsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import styles,{layout} from '../style'
+import Image from 'next/image'
+import { feedback } from "../constents";
+import { features } from 'process';
+import { quotes } from "../assets"
 
-const Testimonials = () =>(
-  <section>
-    
+
+const FeedbackCard =({id,content,name,title,img}:any) => (
+
+  <div className="flex flex-col justify-start  w-[370px] mr-0 sm:mr-5 md:mr-10 px-12 py-10 rounded-[20px] feedback-card">
+
+    <Image src={quotes} alt='quotes'/>
+    <p className='text-white font-poppins font-normal leading-8 text-[18px] my-6'>{content}</p>
+
+      <div className="flex flex-row">
+        <Image src={img} alt='person image' className='h-[48px] w-[48px] rounded-full ' />
+        <div className="flex flex-col ml-4">
+            <h4 className="text-[20px] font-normal font-poppins leading-8 text-white">{name}</h4>
+            <p className='text-dimWhite font-poppins text-[16px] font-normal leading-6'>{title}</p>
+
+        </div>
+      </div>
+
+  </div>
+)
+
+const Testimonials = () => (
+  <section className={`${styles.paddingY} relative flex-col ${styles.flexCenter} `}>
+    <div className='w-full flex  justify-between items-center flex-col md:flex-row mb-6 sm:mb-16 relative z-[1]  '>
+      <h1 className={`${styles.heading2} `}>
+         What People are <br className="sm:block hidden" /> saying about us
+      </h1>
+      <div className={`w-full mt-0 ms:mt-6`}>
+        <p className={`${styles.paragraph} max-w-[450px] text-left`}>Everything you need to accept card payments and grow your business
+          anywhere on the planet.</p>
+      </div>
+
+    </div>
+
+    <div className="flex flex-1 flex-wrap sm:justify-start justify-center items-center feedback-container relative z-[1]">
+      {feedback.map((card) => <FeedbackCard key={card.id} {...card}/> 
+
+        
+      )}
+
+
+    </div>
+
   </section>
 )
 
