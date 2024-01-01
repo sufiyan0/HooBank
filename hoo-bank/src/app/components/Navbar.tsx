186f4b7a03dc29bcf806048876b4bf064f2ac898
Sufiyan_Ahmed <sufiyanlaiq4@gmail.com>
2024-01-01 23:34:34 +0500

    Update Navbar.tsx

--- hoo-bank/src/app/components/Navbar.tsx
+++ hoo-bank/src/app/components/Navbar.tsx
@@ -1,4 +1,4 @@
- 'use client'
+'use client'
 import React, { useState } from 'react';
 import { close, logo, menu } from '../assets'
 import Image from 'next/image'
@@ -32,18 +32,18 @@ const Navbar = () => {
           onClick={() => setToggle((prev) => !prev)}
         />
 
-          <div className={`${toggle    ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute hidden top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
 
-        <ul className='list-none flex sm:hidden flex-col justify-end items-center flex-1'>
-          {navLinks.map((nav, index) => (
-            <li key={nav.id} className={`font-poppins font-normal text-white cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mb-0' : 'mb-4'}`}>
-              <a href={`#${nav.id}`}>{nav.title}</a>
+          <ul className='list-none flex sm:hidden flex-col justify-end items-center flex-1'>
+            {navLinks.map((nav, index) => (
+              <li key={nav.id} className={`font-poppins font-normal text-white cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mb-0' : 'mb-4'}`}>
+                <a href={`#${nav.id}`}>{nav.title}</a>
 
-            </li>
-          ))}
+              </li>
+            ))}
 
-        </ul>
-          </div>
+          </ul>
+        </div>
 
 
 
